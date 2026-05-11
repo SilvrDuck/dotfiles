ff9e76e328de0ca9ffe72becf75c11faa1405d12
SilvrDuck <thibault@thvi.ch>
2026-05-11 16:04:20 +0200

    Add nord themeing to nvim:

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
